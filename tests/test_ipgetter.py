1e84ee4a5147fef608042e529b799162f73c4f59
Tom Vander Aa <tom.vanderaa@gmail.com>
2024-10-04 14:26:13 +0200

    test: some first tests

---
+++ tests/test_ipgetter.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present Tom Vander Aa <tom.vanderaa@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from ipwatch.ipgetter import IPgetter
+
+def test_ipgetter():
+    IPgetter().test()
---
+++ tests/test_ipwatch.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present Tom Vander Aa <tom.vanderaa@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+def test_mocktest():
+    pass
