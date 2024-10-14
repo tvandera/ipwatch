a6b99648b532f973c76ffccf69ee3bc30ddbd8b0
Tom Vander Aa <tom.vanderaa@gmail.com>
2024-10-14 17:55:08 +0200

    simplify config file handling

--- src/ipwatch/ipwatch.py
+++ src/ipwatch/ipwatch.py
@@ -43,72 +43,16 @@ EXAMPLE_CONFIG = dedent("""
     ip_blacklist=192.168.*.*,10.*.*.* # external ips that are not allowed
 """)
 
-class InvalidConfigError(ValueError):
-    def __init__(self, fname = None, missing = None):
-        self.fname = fname
-        self.missing = missing
+NEED_EMAIL_AND_MACHINE = """
+A machine name and email address is required, either in the
+config file or as a command line argument.
+"""
 
-        msg = "\n\nInvalid config file\n"
-        if fname:
-            msg += f"Could not read this file: {fname}\nPlease create it. "
-
-        if missing:
-            msg += f"Missing field: {missing}\n"
-
-        msg += f"Example config file content:\n{EXAMPLE_CONFIG}"
-
-        return super().__init__(msg)
-
-
-@dataclasses.dataclass
-class Config:
-    receiver_email: str
-    machine: str
-    try_count: int = DEFAULT_TRY
-    ip_blacklist: str = DEFAULT_BLACKLIST
-    config_file: str = ""
-    dry_run: bool = False
-
-    @staticmethod
-    def read(fname):
-        logging.info("Reading %s", fname)
-
-        try:
-            with open(fname, "r") as f:
-                content = f.read()
-
-        except FileNotFoundError:
-            raise InvalidConfigError(fname = fname)
-
-        config = ConfigParser()
-        config.read_string("[DEFAULT]\n" + content)
-        config = config["DEFAULT"]
-
-        if "machine" not in config:
-            raise InvalidConfigError(missing = "machine")
-
-        if "receiver_email" not in config:
-            raise InvalidConfigError(missing = "receiver_email")
-
-        return Config(
-            receiver_email= config.get("receiver_email"),
-            machine= config.get("machine"),
-            try_count=config.getint("try_count", DEFAULT_TRY),
-            ip_blacklist=config.get("ip_blacklist", DEFAULT_BLACKLIST),
-            config_file=fname,
-            dry_run=config.getboolean("dry_run"),
-        )
-
-    def merge(self, args):
-        for field in dataclasses.fields(self):
-            if not hasattr(args, field.name):
-                continue
-
-            value = getattr(args, field.name)
-            if value is not None:
-                setattr(self, field.name, value)
-
-        return self
+def read_config(fname):
+    logging.info("Reading %s", fname)
+    config = ConfigParser()
+    config.read_string("[DEFAULT]\n" + open(fname, "r").read())
+    return config["DEFAULT"]
 
 def isipaddr(ipstr):
     """True is ipstr matches x.x.x.x"""
@@ -130,7 +74,7 @@ def getips(try_count, blacklist):
     "Function to return the current, external, IP address"
 
 
-    # try up to config.try_count servers for an IP
+    # try up to args.try_count servers for an IP
     for counter in range(try_count):
 
         # get an IP
@@ -224,6 +168,7 @@ def main():
 
 
     default_config_file = platformdirs.user_config_path("ipwatch") / "config.txt"
+
     parser = argparse.ArgumentParser(description=f"""
 This program gets for your external IP address
 checks it against your "saved" IP address and,
@@ -232,42 +177,49 @@ This is useful for servers at residential locations
 whose IP address may change periodically due to actions
 by the ISP.
 
-To be able to use it, create a config file
-\"{default_config_file.absolute()}\"
+You can also place the configuration for this program
+in a config file (default location:
+\"{default_config_file.absolute()}\")
 with the folllowing info:\n{EXAMPLE_CONFIG}
+
+{NEED_EMAIL_AND_MACHINE}
 """,
     formatter_class=argparse.RawDescriptionHelpFormatter,
 )
 
     parser.add_argument(
         "--config-file",
-        default=default_config_file,
         help="read email-adresses, machine name, blacklist and try count from this file",
     )
 
     parser.add_argument( "--receiver-email", help="receiver email-adress")
     parser.add_argument( "--machine", help="machine name")
-    parser.add_argument( "--try-count", type=int, help="try count")
-    parser.add_argument( "--ip-blacklist", help="ip blacklist")
-
+    parser.add_argument( "--try-count", type=int, default=DEFAULT_TRY, help="try count")
+    parser.add_argument( "--ip-blacklist", default=DEFAULT_BLACKLIST, help="ip blacklist")
 
     parser.add_argument("--repeat", type=int, default=-1, help="repeat every N seconds")
     parser.add_argument("--dry-run", action="store_true", help="do not send email")
     parser.add_argument("--verbose", help="increase logging verbosity", action="store_const", const=logging.INFO)
     parser.add_argument("--force", help="always report external ip, even if unchanged", action="store_true")
+
+    args = parser.parse_args()
+    if args.config_file is not None:
+        parser.set_defaults(**read_config(args.config_file))
+    elif default_config_file.exists():
+        parser.set_defaults(**read_config(default_config_file))
     args = parser.parse_args()
 
-    logging.basicConfig(level=args.verbose)
+    if not args.receiver_email or not args.machine:
+        parser.error(NEED_EMAIL_AND_MACHINE)
 
-    # read config file
-    config = Config.read(args.config_file).merge(args)
+    logging.basicConfig(level=args.verbose)
 
     save_ip_path = platformdirs.user_cache_path("ipwatch") / "saved_ip.txt"
 
     while True:
         old_external_ip, old_local_ip = getoldips(save_ip_path)
         curr_external_ip, curr_local_ip, server = getips(
-            int(config.try_count), config.ip_blacklist
+            int(args.try_count), args.ip_blacklist
         )
 
         # check to see if the IP address has changed
@@ -282,8 +234,8 @@ with the folllowing info:\n{EXAMPLE_CONFIG}
                 curr_external_ip,
                 curr_local_ip,
                 server,
-                config.receiver_email,
-                config.machine,
+                args.receiver_email,
+                args.machine,
                 dry_run = args.dry_run,
             )
 
