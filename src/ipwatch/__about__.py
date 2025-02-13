7d11f4d97f0194430b854ae122e18dbd83de847a
Tom Vander Aa <vanderaa@imec.be>
2025-02-13 22:01:43 +0100

    docs: vanderaa@imec.be -> tom.vanderaa@gmail.com

--- src/ipwatch/__about__.py
+++ src/ipwatch/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-present Tom Vander Aa <vanderaa@imec.be>
+# SPDX-FileCopyrightText: 2024-present Tom Vander Aa <tom.vanderaa@gmail.com>
 #
 # SPDX-License-Identifier: MIT
 __version__ = "0.0.1"
